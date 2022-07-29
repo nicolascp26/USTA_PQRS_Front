dd64a674ffb8630c550dfac600b4caa49f4677a9
Nicolas Carreno <nicox2600@gmail.com>
2022-07-28 22:30:05 -0500

    Several Message Module fixes

--- src/app/modelos/mensaje.ts
+++ src/app/modelos/mensaje.ts
@@ -1,32 +1,33 @@
 export class Mensaje {
   public mensajeId: number;
-  public mensajeCodpadre?: number;
+  public mensajeCodpadre: number;
+  public mensajeUsuario: number;
   public mensajeTitulo: string;
   public mensajeDetalle: string;
   public mensajeFecha: string;
-  public mensajeHora:string;
   public mensajeEstado?: number;
   public mensajePrioridad?: number;
-  public usuarioNombres?:string;
-  public usuarioApellidos?:string;
-  public mensajeTipoId?: number;
+  public usuarioNombres?: string;
+  public usuarioApellidos?: string;
+  public tipoClase?: string;
+  public tipoNombre?: string;
   public constructor(
     cod: number,
+    codPad: number,
+    msjUsu: number,
     tit: string,
     det: string,
-    fec:string,
-    hor:string,
+    fec: string,
     est: number,
-    pri: number,
-    tipo: number
+    pri: number
   ) {
     this.mensajeId = cod;
+    this.mensajeCodpadre = codPad;
+    this.mensajeUsuario = msjUsu;
     this.mensajeTitulo = tit;
     this.mensajeDetalle = det;
     this.mensajeFecha = fec;
-    this.mensajeHora = hor;
     this.mensajeEstado = est;
     this.mensajePrioridad = pri;
-    this.mensajeTipoId = tipo;
   }
 }
