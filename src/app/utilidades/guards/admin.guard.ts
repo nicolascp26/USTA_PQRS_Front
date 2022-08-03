36cf5c1495bbecebd8f350dd44551188c6b33cff
Nicolas Carreno <nicox2600@gmail.com>
2022-08-03 15:55:23 -0500

    Implement Routes protection

---
+++ src/app/utilidades/guards/admin.guard.ts
@@ -0,0 +1,36 @@
+import { AccesoService } from '../../servicios/acceso.service';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateChild,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivateChild {
+  constructor(
+    private accesoService: AccesoService,
+    private location: Location
+  ) {}
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.accesoService.accederRutasAdmin()) {
+      return true;
+    } else {
+      this.location.back();
+      return false;
+    }
+  }
+}
