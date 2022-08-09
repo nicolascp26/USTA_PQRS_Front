c82e00d087c6e44d61588d3bec2ef424e631eafe
Nicolas Carreno <nicox2600@gmail.com>
2022-08-08 21:19:06 -0500

    Roles y Estad. Inicio

--- src/app/modulos/privado/administrador/control/control.module.ts
+++ src/app/modulos/privado/administrador/control/control.module.ts
@@ -5,9 +5,11 @@ import { ControlRoutingModule } from './control-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
 import { ErrorSeguridadComponent } from './error-seguridad/error-seguridad.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { ProgressbarModule,ProgressbarConfig } from 'ngx-bootstrap/progressbar';
 
 @NgModule({
   declarations: [InicioComponent, ErrorSeguridadComponent, PerfilComponent],
-  imports: [CommonModule, FormsModule, ControlRoutingModule],
+  imports: [CommonModule, FormsModule, ProgressbarModule, ControlRoutingModule],
+  providers:[ProgressbarConfig]
 })
 export class ControlModule {}
