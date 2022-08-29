bc0f3edea8cbf2c95dc64bdb0081fbc25e587d1a
Nicolas Carreno <nicox2600@gmail.com>
2022-08-29 13:20:40 -0500

    Upload File: New Request Estudiante

--- src/app/modulos/privado/estudiante/solicitudes/mensaje-crear/mensaje-crear.component.ts
+++ src/app/modulos/privado/estudiante/solicitudes/mensaje-crear/mensaje-crear.component.ts
@@ -1,3 +1,4 @@
+import { AnexosService } from './../../../../../servicios/anexos.service';
 import { Router } from '@angular/router';
 import { mostrarMensaje } from 'src/app/utilidades/mensajes/mensajes-toast.func';
 import { ToastrService } from 'ngx-toastr';
@@ -20,18 +21,23 @@ import { NgForm } from '@angular/forms';
 export class MensajeCrearComponent implements OnInit {
   public objMensaje: Mensaje;
   public arregloTipos: Tipo[];
+  public envioFinalizado: boolean;
 
   //Atributos modales
   public modalTitulo: string;
   public modalContenido: string;
   public modalRef: BsModalRef;
 
+  //Atributos de anexos
+  public tmpFile: any;
+  public anexoSeleccionado: boolean;
+  public arregloAnexos: any;
+
   //Atributos consumo servicios
   public tmp: any;
   public miSuscripcion: Subscription;
   public cargaFinalizada: boolean;
 
-  public searchBar = '';
   public defaultTipo = 'Seleccione el tipo';
 
   public arregloTipoClases = [
@@ -45,6 +51,7 @@ export class MensajeCrearComponent implements OnInit {
     private mensajesService: MensajesService,
     private accesoService: AccesoService,
     private tipoService: TipoService,
+    private anexoService: AnexosService,
     public modalService: BsModalService,
     private toastr: ToastrService,
     public router: Router
@@ -52,6 +59,11 @@ export class MensajeCrearComponent implements OnInit {
     //Inicializar atributos requeridos
     this.objMensaje = this.inicializarMensaje();
     this.arregloTipos = [];
+    this.envioFinalizado = true;
+
+    //Inicializar atributos de subir anexos
+    this.anexoSeleccionado = false;
+    this.arregloAnexos = [];
 
     //Inicializar consumo de servicios
     this.miSuscripcion = this.tmp;
@@ -69,19 +81,30 @@ export class MensajeCrearComponent implements OnInit {
   }
 
   public inicializarMensaje(): Mensaje {
-    return new Mensaje(0, 0, 0,'','','',0);
+    return new Mensaje(0, 0, 0, '', '', '', 0);
   }
 
   public crearSolicitud(formulario: NgForm): void {
-    console.log(this.objMensaje);
+    this.envioFinalizado = false;
     this.miSuscripcion = this.mensajesService
       .registrarSolicitud(this.objMensaje)
       .pipe(
-        map((respuesta) => {
-          mostrarMensaje('success', 'Solicitud registrada', 'Exito', this.toastr);
+        map((respuesta: any) => {
+          mostrarMensaje(
+            'success',
+            'Solicitud registrada',
+            'Exito',
+            this.toastr
+          );
+          if (this.anexoSeleccionado) {
+            this.subirAnexo(respuesta.resultado.mensajeId);
+          }
           formulario.reset();
           this.router.navigate(['/estudiante/solicitudes']);
           return respuesta;
+        }),
+        finalize(() => {
+          this.envioFinalizado = true;
         }),
         catchError((miError) => {
           mostrarMensaje(
@@ -105,11 +128,55 @@ export class MensajeCrearComponent implements OnInit {
         }),
         finalize(() => {
           this.cargaFinalizada = true;
-          this.searchBar = '';
         })
       )
       .subscribe(observadorAny);
   }
 
+  //Metodos de subir archivo
+  public seleccionarAnexo(event: any): void {
+    let caja = event.target.files[0];
+    if (!caja || caja.length == 0) {
+      this.anexoSeleccionado = false;
+      return;
+    }
+    if (caja.size > 2097152) {
+      this.tmpFile = this.tmp;
+      mostrarMensaje(
+        'error',
+        'El archivo pesa mas de 2MB',
+        'Advertencia',
+        this.toastr
+      );
+      this.anexoSeleccionado = false;
+      return;
+    } else {
+      this.tmpFile = {
+        fileRaw: caja,
+        fileName: caja.name,
+        fileType: caja.type,
+      };
+      this.anexoSeleccionado = true;
+    }
+  }
 
+  public subirAnexo(mensajeId: number): void {
+    const body = new FormData();
+    body.append('myFile', this.tmpFile.fileRaw, this.tmpFile.fileName);
+    body.append('mensajeId', mensajeId.toString());
+    this.miSuscripcion = this.anexoService
+      .subirAnexo(body)
+      .pipe(
+        map((respuesta) => {
+          return respuesta;
+        }),
+        finalize(() => {
+          this.tmpFile = this.tmp;
+        }),
+        catchError((miError) => {
+          throw miError;
+        })
+      )
+      .subscribe(observadorAny);
+  }
 }
